648b0b7a508b7f7d92e675cfaa43fff3c6da306f
SeryiBaran <seryibaranminepy@yandex.ru>
2022-07-03 19:37:30 +0300

    Fix tabulation

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import { PISTR} from "./utils/pi.js";
+import { PISTR } from "./utils/pi.js";
 
 export const URandom = {
-	int: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
-	float: (min, max) => Math.random() * (max - min + 1) + min,
-	bool: () => Math.random() < 0.5,
-	arrElem: (arr) => arr[Math.floor(Math.random() * numPool.length)],
-	arrOfNumbers(len, min, max) {
-		let result = [];
-		for (let i = 0; i < len; i++) {
-			let temp = this.int(min, max);
-			if (result.indexOf(temp) === -1) {
-				result.push(temp);
-			} else {
-				i--;
-			}
-		}
-		return result;
-	},
+  int: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+  float: (min, max) => Math.random() * (max - min + 1) + min,
+  bool: () => Math.random() < 0.5,
+  arrElem: (arr) => arr[Math.floor(Math.random() * numPool.length)],
+  arrOfNumbers(len, min, max) {
+    let result = [];
+    for (let i = 0; i < len; i++) {
+      let temp = this.int(min, max);
+      if (result.indexOf(temp) === -1) {
+        result.push(temp);
+      } else {
+        i--;
+      }
+    }
+    return result;
+  },
 };
 
 export const UMath = {
-	pi: (len) => {
-		if (len > PISTR.length || len <= 0) {
-			throw "UUJS: Error: Invalid argument \"length\" of method UMath.pi!";
-			return;
-		}
-		return `3.${PISTR.slice(0, len)}`;
-	},
+  pi: (len) => {
+    if (len > PISTR.length || len <= 0) {
+      throw 'UUJS: Error: Invalid argument "length" of method UMath.pi!';
+      return;
+    }
+    return `3.${PISTR.slice(0, len)}`;
+  },
 };
